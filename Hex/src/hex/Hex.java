1310316315838d02463e677cc3c8be13a34d6c93
einmichael <>
2017-01-04 14:30:40 +0100

    new commit

--- Hex/src/hex/Hex.java
+++ Hex/src/hex/Hex.java
@@ -5,17 +5,41 @@
  */
 package hex;
 
+import java.awt.Dimension;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 /**
  *
  * @author michael.faust
  */
-public class Hex {
+public class Hex extends JFrame {
 
+    public static Hex hex;
+    public Hex(String s){
+        super(s);
+        this.add(new JPanel(){
+            
+        });
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        hex =  new Hex("Sim");
+        hex.setLayout(null);
+        hex.setLocation(250,250);
+        hex.setPreferredSize(new Dimension(500,500));
+        hex.pack();
+        hex.setVisible(true);
+        
+        hex.addWindowListener(new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent e){
+                System.exit(0);
+            }
+        });
     }
-    
-}
+ }
