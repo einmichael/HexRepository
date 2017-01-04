829996009e7ce200c20d4a1de5df60a961c58e4f
einmichael <>
2017-01-04 15:08:19 +0100

    new commit

--- Hex/src/hex/Hex.java
+++ Hex/src/hex/Hex.java
@@ -8,6 +8,7 @@ package hex;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Insets;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import javax.swing.JFrame;
@@ -32,27 +33,26 @@ public class Hex extends JFrame {
      */
     public static void main(String[] args) {
         hex =  new Hex("Sim");
-        hex.setLayout(null);
         hex.setLocation(250,250);
-        hex.setPreferredSize(new Dimension(500,500));
-        hex.pack();
         hex.setVisible(true);
-        hex.repaint();
+        Insets insets = hex.getInsets();
+        hex.setPreferredSize(new Dimension(
+                500 + insets.left + insets.right,
+                500 + insets.top + insets.bottom));
+                
         panel=new JPanel(){
             @Override
             public void paintComponent(Graphics g){
                 super.paintComponent(g);
                 g.setColor(Color.red);
                 g.fillRect(100,100,200,400);
-                System.out.println("ASDF");
+                                
             }
         };
         
         hex.add(panel);
-        panel.setVisible(true);
-        hex.repaint();
-        panel.repaint();
-        
+        hex.pack();
+               
         hex.addWindowListener(new WindowAdapter(){
             @Override
             public void windowClosing(WindowEvent e){
